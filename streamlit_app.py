9d74f52b70ada167b3aca8d4d323ba0b1b95d421
YujiyMarks <43303495+YujiyMarks@users.noreply.github.com>
2025-07-02 21:05:58 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,17 @@
 import streamlit as st
+from streamlit.logger import get_logger
+import sqlite3
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+LOGGER = get_logger(__name__)
+
+def run():
+    st.set_page_config(page_title="EMBRAPA",page_icon=":gear:",)
+    st.title("Embrapa Agricultura Digital")
+    st.sidebar.success("HOME")
+
+    #dados = sqlite3.connect('bd_app.db')
+    #cursor = dados.cursor()
+
+if __name__ == "__main__":
+    run()
+
